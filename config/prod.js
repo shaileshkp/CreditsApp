34141621e147b77e992a77dbc96c020778cff252
Shailesh Pandey <shaileshkp001@gmail.com>
2017-12-11 22:26:49 +0530

    Added heroku setup of auth with different keys

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+// production enviournment
+
+module.exports = {
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+  };
+  
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURL);
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
